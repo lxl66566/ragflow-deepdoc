9bfe8f200db8cabc454cae95cf0882113c1de440
lxl66566 <lxl66566@gmail.com>
2025-04-07 11:29:25 +0800

    only for deepdoc
    Signed-off-by: lxl66566 <lxl66566@gmail.com>

--- deepdoc/vision/t_ocr.py
+++ deepdoc/vision/t_ocr.py
@@ -16,30 +16,34 @@
 
 import os
 import sys
-sys.path.insert(
-    0,
-    os.path.abspath(
-        os.path.join(
-            os.path.dirname(
-                os.path.abspath(__file__)),
-            '../../')))
 
-from deepdoc.vision.seeit import draw_box
-from deepdoc.vision import OCR, init_in_out
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../")))
+
 import argparse
+
 import numpy as np
 import trio
 
+from deepdoc.vision import OCR, init_in_out
+from deepdoc.vision.seeit import draw_box
+
 # os.environ['CUDA_VISIBLE_DEVICES'] = '0,2' #2 gpus, uncontinuous
-os.environ['CUDA_VISIBLE_DEVICES'] = '0' #1 gpu
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"  # 1 gpu
 # os.environ['CUDA_VISIBLE_DEVICES'] = '' #cpu
 
 
 def main(args):
-    import torch.cuda
+    use_cuda = False
+    try:
+        import torch.cuda
 
-    cuda_devices = torch.cuda.device_count()
-    limiter = [trio.CapacityLimiter(1) for _ in range(cuda_devices)] if cuda_devices > 1 else None
+        cuda_devices = torch.cuda.device_count()
+        limiter = [trio.CapacityLimiter(1) for _ in range(cuda_devices)] if cuda_devices > 1 else None
+        if cuda_devices > 1:
+            use_cuda = True
+    except ImportError:
+        print("CUDA not found, using CPU")
+
     ocr = OCR()
     images, outputs = init_in_out(args)
 
@@ -47,19 +51,15 @@ def main(args):
         print("Task {} start".format(i))
         bxs = ocr(np.array(img), id)
         bxs = [(line[0], line[1][0]) for line in bxs]
-        bxs = [{
-            "text": t,
-            "bbox": [b[0][0], b[0][1], b[1][0], b[-1][1]],
-            "type": "ocr",
-            "score": 1} for b, t in bxs if b[0][0] <= b[1][0] and b[0][1] <= b[-1][1]]
-        img = draw_box(images[i], bxs, ["ocr"], 1.)
+        bxs = [{"text": t, "bbox": [b[0][0], b[0][1], b[1][0], b[-1][1]], "type": "ocr", "score": 1} for b, t in bxs if b[0][0] <= b[1][0] and b[0][1] <= b[-1][1]]
+        img = draw_box(images[i], bxs, ["ocr"], 1.0)
         img.save(outputs[i], quality=95)
-        with open(outputs[i] + ".txt", "w+", encoding='utf-8') as f:
+        with open(outputs[i] + ".txt", "w+", encoding="utf-8") as f:
             f.write("\n".join([o["text"] for o in bxs]))
 
         print("Task {} done".format(i))
 
-    async def __ocr_thread(i, id, img, limiter = None):
+    async def __ocr_thread(i, id, img, limiter=None):
         if limiter:
             async with limiter:
                 print("Task {} use device {}".format(i, id))
@@ -68,10 +68,10 @@ def main(args):
             __ocr(i, id, img)
 
     async def __ocr_launcher():
-        if cuda_devices > 1:
+        if use_cuda:
             async with trio.open_nursery() as nursery:
                 for i, img in enumerate(images):
-                    nursery.start_soon(__ocr_thread, i, i % cuda_devices, img, limiter[i % cuda_devices])
+                    nursery.start_soon(__ocr_thread, i, i % cuda_devices, img, limiter[i % cuda_devices])  # type: ignore
                     await trio.sleep(0.1)
         else:
             for i, img in enumerate(images):
@@ -84,10 +84,7 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--inputs',
-                        help="Directory where to store images or PDFs, or a file path to a single image or PDF",
-                        required=True)
-    parser.add_argument('--output_dir', help="Directory where to store the output images. Default: './ocr_outputs'",
-                        default="./ocr_outputs")
+    parser.add_argument("--inputs", help="Directory where to store images or PDFs, or a file path to a single image or PDF", required=True)
+    parser.add_argument("--output_dir", help="Directory where to store the output images. Default: './ocr_outputs'", default="./ocr_outputs")
     args = parser.parse_args()
     main(args)
